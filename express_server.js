f32ae29515165668bc8503c206c3de9c11d55d5f
Gary <garyyue2017@gmail.com>
2019-08-07 21:24:37 +0000

    limited shorten url access to registered users

--- express_server.js
+++ express_server.js
@@ -46,6 +46,9 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  if(!req.cookies["user_id"]){
+    return res.redirect("/login");
+  }
   let templateVars = {
     user: users[req.cookies["user_id"]]
   }
